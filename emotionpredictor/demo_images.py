236038229745b5d968c49b922989a4beb087de8e
robin szymczak <robin@tsf-436-wpa-1-141.epfl.ch>
2022-02-08 14:20:34 +0100

    Web image emotion prediction

--- emotionpredictor/demo_images.py
+++ emotionpredictor/demo_images.py
@@ -1,61 +1,108 @@
-import datetime
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import torch
+import torch.nn as nn
+import numpy as np
+import plotly.express as px
+import os
+import random 
+from PIL import Image
+import clip
+import torch
+from PIL import Image
+import requests
 
-from dash import Dash, dcc, html
-from dash.dependencies import Input, Output, State
+device = "cpu"
+def get_iterator(path, preprocess) :
+    def iterator():
+        for image in os.listdir(path):
+            if image[-4:] != ".jpg":
+                continue
+            img_path = osp.join(path,image)
+            yield preprocess(Image.open(img_path)).unsqueeze(0)
+    return iterator()
 
+
+
+class SLP(nn.Module):
+    def __init__(self,input_size = 512, output_size = 9):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(input_size, output_size)
+        )
+        
+    def forward(self, x):
+        fp = self.layers(x.float())
+        return fp
+
+MODEL_PATH = "../../code/C-RN50x16"
+emotion = SLP(768)
+emotion.load_state_dict(torch.load(MODEL_PATH, map_location=torch.device('cpu')))
+
+ARTEMIS_EMOTIONS = ['amusement',
+ 'awe',
+ 'contentment',
+ 'excitement',
+ 'anger',
+ 'disgust',
+ 'fear',
+ 'sadness',
+ 'something else']
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+@torch.no_grad()
+def get_text_encoding(texts, text_encoder):
+    text = clip.tokenize(texts).to(device)
+    text_features = text_encoder(text)
+    return text_features
+
+@torch.no_grad()
+def get_img_encoding(img, img_encoder):
+    return img_encoder(img)
+
+def preprocess_img(img):
+    return preprocess(img).unsqueeze(0)
 
 app.layout = html.Div([
-    dcc.Upload(
-        id='upload-image',
-        children=html.Div([
-            'Drag and Drop or ',
-            html.A('Select Files')
-        ]),
-        style={
-            'width': '100%',
-            'height': '60px',
-            'lineHeight': '60px',
-            'borderWidth': '1px',
-            'borderStyle': 'dashed',
-            'borderRadius': '5px',
-            'textAlign': 'center',
-            'margin': '10px'
-        },
-        # Allow multiple files to be uploaded
-        multiple=True
+    html.H1(
+        children='Emotion Predictor : Image',
+        style={'textAlign': 'center'}
     ),
-    html.Div(id='output-image-upload'),
-])
+    html.Br(),
+    html.Div([
+        dcc.Graph(id='img'),
+        dcc.Graph(id='graph')
+    ],style={'display': 'flex', 'flex-direction': 'row', "border" : "solid", "border-color" : "#F8F9FD"}),
+    html.Div([
+        html.H6("Provide the url of an image :"),
+        html.Div([
+            dcc.Input(id='my-input', value='https://uploads5.wikiart.org/images/tivadar-kosztka-csontvary/castellamare-di-stabia-1902.jpg', type='url')
+        ])
+    ], style = {'textAlign' : 'center'}),
+], style = {"background-color" : "#F5F6F7"})
 
-def parse_contents(contents, filename, date):
-    return html.Div([
-        html.H5(filename),
-        html.H6(datetime.datetime.fromtimestamp(date)),
+@app.callback(
+    [Output(component_id='img', component_property='figure'),
+    Output(component_id='graph', component_property='figure')],
+    Input(component_id='my-input', component_property='value')
+)
+def update_output_div(input_value):
+    
+    img = Image.open(requests.get(input_value, stream=True).raw)
+    features = get_img_encoding(preprocess_img(img), clip_model.visual)
+    fig_bars = px.bar_polar(r = emotion(features).softmax(1).tolist()[0], theta = ARTEMIS_EMOTIONS, color = ARTEMIS_EMOTIONS)
+    fig_bars.update_layout(transition_duration=1000, transition = {'easing': 'cubic-in-out'})
+    fig_img = px.imshow(img)
+    fig_img.update_layout(transition_duration=1000, transition = {'easing': 'cubic-in-out'})
+    return fig_img, fig_bars
 
-        # HTML images accept base64 encoded strings in the same format
-        # that is supplied by the upload
-        html.Img(src=contents),
-        html.Hr(),
-        html.Div('Raw Content'),
-        html.Pre(contents[0:200] + '...', style={
-            'whiteSpace': 'pre-wrap',
-            'wordBreak': 'break-all'
-        })
-    ])
-
-@app.callback(Output('output-image-upload', 'children'),
-              Input('upload-image', 'contents'),
-              State('upload-image', 'filename'),
-              State('upload-image', 'last_modified'))
-def update_output(list_of_contents, list_of_names, list_of_dates):
-    if list_of_contents is not None:
-        children = [
-            parse_contents(c, n, d) for c, n, d in
-            zip(list_of_contents, list_of_names, list_of_dates)]
-        return children
+clip_model, preprocess = clip.load("RN50x16")
+clip_model.eval();
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, port = 8050)
